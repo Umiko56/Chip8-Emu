99d457d29044332ddb215591e3c86b1b09b9ca79
Tsuki Superior <tsukisuperior@tutanota.com>
2021-04-23 13:54:59 -0500

    inproved this code

--- src/emulator.c
+++ src/emulator.c
@@ -5,51 +5,64 @@
 #include "graphics.h"
 #include "emulator.h"
 
-
-
 void update_event(struct s_input *input)
 {
     SDL_Event event;
-    while(SDL_PollEvent(&event))
+    while (SDL_PollEvent(&event))
     {
-        if(event.type == SDL_QUIT)
-            input->quit = SDL_TRUE;
-        else if(event.type == SDL_KEYDOWN)
-            input->key[event.key.keysym.scancode] = SDL_TRUE;
-        else if(event.type == SDL_KEYUP)
-            input->key[event.key.keysym.scancode] = SDL_FALSE;
-        else if(event.type == SDL_MOUSEMOTION)
+        switch (event.type)
         {
+        case SDL_QUIT:
+            input->quit = SDL_TRUE;
+            break;
+
+        case SDL_KEYDOWN:
+            input->key[event.key.keysym.scancode] = SDL_TRUE;
+            break;
+
+        case SDL_KEYUP:
+            input->key[event.key.keysym.scancode] = SDL_FALSE;
+            break;
+
+        case SDL_MOUSEMOTION:
             input->x = event.motion.x;
             input->y = event.motion.y;
             input->xrel = event.motion.xrel;
             input->yrel = event.motion.yrel;
-        }
-        else if(event.type == SDL_MOUSEWHEEL)
-        {
+            break;
+
+        case SDL_MOUSEWHEEL:
             input->xwheel = event.wheel.x;
             input->ywheel = event.wheel.y;
-        }
-        else if(event.type == SDL_MOUSEBUTTONDOWN)
-              input->mouse[event.button.button] = SDL_TRUE;
-        else if(event.type == SDL_MOUSEBUTTONUP)
+            break;
+
+        case SDL_MOUSEBUTTONDOWN:
+            input->mouse[event.button.button] = SDL_TRUE;
+            break;
+
+        case SDL_MOUSEBUTTONUP:
             input->mouse[event.button.button] = SDL_FALSE;
-        else if(event.type == SDL_WINDOWEVENT)
-            if(event.window.event == SDL_WINDOWEVENT_RESIZED)
+            break;
+
+        case SDL_WINDOWEVENT:
+            if (event.window.event == SDL_WINDOWEVENT_RESIZED)
+            {
                 input->resize = SDL_TRUE;
+            }
+            break;
+
+        }
     }
 }
 
-
-
 int initialize_SDL(void)
 {
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         fprintf(stderr, "Error SDL_Init: %s.\n", SDL_GetError());
-        return -1;
+        return EXIT_FAILURE;
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 void destroy_SDL(void)
@@ -62,10 +75,10 @@ int initialize_emulator(struct s_emulator *emulator)
     int status = -1;
     initialize_cpu(&emulator->cpu);
     memset(&emulator->in, 0, sizeof(emulator->in));
-    if(0 == initialize_SDL())
+    if (0 == initialize_SDL())
     {
         int status = initialize_screen(&emulator->screen);
-        if(0 > status)
+        if (0 > status)
             destroy_SDL();
     }
     return status;
@@ -79,7 +92,7 @@ void destroy_emulator(struct s_emulator *emulator)
 
 void emulate(struct s_emulator *emulator)
 {
-    while(!emulator->in.quit)
+    while (!emulator->in.quit)
     {
         update_event(&emulator->in);
         update_screen(&emulator->screen);
--- src/memory.c
+++ src/memory.c
@@ -11,21 +11,21 @@ void initialize_cpu(struct s_cpu *cpu)
 
 void count(struct s_cpu *cpu)
 {
-    if(cpu->sys_counter > 0)
+    if (cpu->sys_counter > 0)
         cpu->sys_counter--;
-    if(cpu->sound_counter > 0)
+    if (cpu->sound_counter > 0)
         cpu->sound_counter--;
 }
 
 int load_rom(struct s_cpu *cpu, const char path[])
 {
     FILE *rom = fopen(path, "rb");
-    if(!rom)
+    if (!rom)
     {
         perror("Error fopen:");
         return EXIT_FAILURE;
     }
-    fread(&cpu->memory[START_ADDRESS], sizeof(Uint8) * (MEMORY_SIZE - START_ADDRESS), 1, rom);
+    fread(&cpu->memory[START_ADDRESS], sizeof(uint8_t) * (MEMORY_SIZE - START_ADDRESS), 1, rom);
     fclose(rom);
     return EXIT_SUCCESS;
 }
