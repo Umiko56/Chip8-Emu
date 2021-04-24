fe820342baad64193f8b1abe8e83a633d6339522
Tsuki Superior <tsukisuperior@tutanota.com>
2021-04-24 18:06:39 -0500

    fixed decoding error

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -21,51 +21,99 @@ void Cpu::execute()
 {
 
     uint16_t inst = (ram[instruction_pointer] << 8 | ram[instruction_pointer + 1]);
-    instruction_pointer++;
+    instruction_pointer += 2;
 
     // Unpack the instruction
     uint8_t nibbles[4];
-    nibbles[0] = (uint8_t)(inst & 0xf000) >> 12;
-    nibbles[1] = (uint8_t)(inst & 0x0f00) >> 8;
-    nibbles[2] = (uint8_t)(inst & 0x00f0) >> 4;
-    nibbles[3] = (uint8_t)(inst & 0x000f);
+    nibbles[0] = (inst & 0xf000) >> 12;
+    nibbles[1] = (inst & 0x0f00) >> 8;
+    nibbles[2] = (inst & 0x00f0) >> 4;
+    nibbles[3] = (inst & 0x000f);
 
     switch (nibbles[0])
     {
     case 0x0:
+        switch (nibbles[2])
+        {
+        case 0xE:
+            switch (nibbles[3])
+            {
+
+            // CLS
+            case 0x0:
+                break;
+
+            // RET
+            case 0xE:
+                break;
+
+            // Unknown
+            default:
+
+                break;
+            }
+
+        default:
+            // SYS addr
+            break;
+        }
         break;
+    //JP addr
     case 0x1:
         instruction_pointer = inst & 0x0fff;
         break;
+
+    //CALL addr
     case 0x2:
         break;
+
+    //SE Vx, byte
     case 0x3:
         if (nibbles[1] == (inst & 0xff))
         {
-            instruction_pointer++;
+            instruction_pointer += 2;
         }
         break;
+
+    //SNE Vx, byte
     case 0x4:
         if (nibbles[1] != (inst & 0xff))
         {
-            instruction_pointer++;
+            instruction_pointer += 2;
         }
         break;
+
+    //SE Vx, Vy
     case 0x5:
         switch (nibbles[4])
         {
         case 0x0:
             if (nibbles[1] != nibbles[2])
             {
-                instruction_pointer++;
+                instruction_pointer += 2;
             }
             break;
         default:
-            std::cerr << "ERROR: INVALID INSTRUCTION: " << std::hex << instruction_pointer << ":" << inst << std::dec;
             break;
         }
         break;
+
+    //LD Vx, byte
+    case 0x6:
+        switch (nibbles[3])
+        {
+        //LD Vx, byte
+        case 0:
+            general_registers[nibbles[1]] = general_registers[nibbles[2]];
+
+        //Unknown
+        default:
+            break;
+        }
+        break;
+    case 0x7:
+        break;
     default:
-        std::cerr << "ERROR: INVALID INSTRUCTION: " << std::hex << instruction_pointer << ":" << inst << std::dec;
+        break;
     }
 }
